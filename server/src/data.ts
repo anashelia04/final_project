41a814d0f6bbe596e67f08f715f617188fac795a
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2025-06-01 18:47:39 +0400

    add sample data in backend

---
+++ server/src/data.ts
@@ -0,0 +1,20 @@
+import { VolunteerOpportunity } from "./types/VolunteerOpportunity";
+
+export const opportunities: VolunteerOpportunity[] = [
+  {
+    id: 1,
+    title: "Beach Cleanup",
+    description: "Help clean the local beach.",
+    date: "2025-06-10",
+    location: "San Diego, CA",
+    category: "Environment"
+  },
+  {
+    id: 2,
+    title: "Tutoring Kids",
+    description: "Provide homework help to kids in need.",
+    date: "2025-06-15",
+    location: "Atlanta, GA",
+    category: "Education"
+  }
+];
