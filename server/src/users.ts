d76fdd80fc870d2d673d81b9fc5e6654dc71b62b
kiu_shelia.ana <shelia.ana@kiu.edu.ge>
2025-06-29 16:39:59 +0400

    Users can now join opportunities

--- server/src/users.ts
+++ server/src/users.ts
@@ -3,4 +3,5 @@ import { User } from '../../shared/types/User';
 export const users: User[] = [
   { id: 'user-1', username: 'organizer_alice', password: 'password123' },
   { id: 'user-2', username: 'volunteer_bob', password: 'password456' },
+  { id: 'user-3', username: 'admin_charlie', password: 'password789' },
 ];
